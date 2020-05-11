74ccfe8a424c4559830c46b60c357bad68dc26ba
Jens Mertelmeyer <jens@mertelmeyer.de>
2020-05-11 15:59:41 +0200

    Add MergePatch(..) method (#2)
    * Add .DPROJ file for test project
    
    * Add TJSONHelper.MergePatch(..)
    
    JSON Merge Patch, according to RFC 7396
    https://tools.ietf.org/html/rfc7396

--- Utils.JSON.Test.pas
+++ Utils.JSON.Test.pas
@@ -17,6 +17,93 @@ type
     procedure ObjectEquals_TextField();
     procedure ObjectEquals_DifferentOrder();
     procedure ObjectEquals_NestedObject();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {"a":"b"}       {"a":null}      {}
+	procedure MergePatch_RemovesNullValues1();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {"a":"b",       {"a":null}      {"b":"c"}
+	//    "b":"c"}
+	procedure MergePatch_RemovesNullValues2();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {"a": {         {"a": {         {"a": {
+	//     "b": "c"}       "b": "d",       "b": "d"
+	//   }                 "c": null}      }
+	//                   }               }
+	procedure MergePatch_RemoveSomethingThatDoesNotExist();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {"a":"foo"}     null            null
+	procedure MergePatch_RemovesEverything();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {"a":"b"}       {"a":"c"}       {"a":"c"}
+	procedure MergePatch_ReplacesValue();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {"a":["b"]}     {"a":"c"}       {"a":"c"}
+	procedure MergePatch_ReplacesArray();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {"a":"c"}       {"a":["b"}}       {"a":["b"]}
+	procedure MergePatch_ReplacesValueWithArray();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {"a":"b"}       {"b":"c"}       {"a":"b",
+	//                                    "b":"c"}
+	procedure MergePatch_AddsPair();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   ["a","b"]       ["c","d"]       ["c","d"]
+	procedure MergePath_ReplacesArrayWithOtherArray();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {"a":"b"}       ["c"]           ["c"]
+	procedure MergePath_ReplacesObjectWithArray();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   [1,2]           {"a":"b",       {"a":"b"}
+	//                    "c":null}
+	procedure MergePath_ReplacesArrayWithObject();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {"a":"foo"}     "bar"           "bar"
+	procedure MergePatch_ReplacesObjectWithString();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {"a": [         {"a": [1]}      {"a": [1]}
+	//     {"b":"c"}
+	//    ]
+	//   }
+	procedure MergePatch_ReplacesArrayWithArray();
+
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {"e":null}      {"a":1}         {"e":null,
+	//                                    "a":1}
+	procedure MergePatch_AddsPair_ExistingNullValue();
+	//   ORIGINAL        PATCH            RESULT
+	//   ------------------------------------------
+	//   {}              {"a":            {"a":
+	//                    {"bb":           {"bb":
+	//                     {"ccc":          {}}}
+	//					  null}}}
+	procedure MergePatch_RemovesSomethingThatDoesNotExist2();
   end;
 
 implementation uses System.Json, System.Json.Builders, System.Json.Writers, System.SysUtils;
@@ -102,6 +189,359 @@ begin
   finally
     a.Free(); b.Free(); c.Free();
   end;
+end;
+
+procedure TestTJsonHelper.MergePatch_AddsPair();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('{"a":"b"}');
+		patch := TJSONObject.ParseJSONValue('{"b": "c"}');
+		result := TJSONHelper.MergePatch(target, patch);
+		Check(result <> patch, 'result <> patch');
+		Check(result <> target, 'result <> target');
+		Check(result is TJSONObject, 'result is TJSONObject');
+		//   {"a":"b"}       {"b":"c"}       {"a":"b",
+		//                                    "b":"c"}
+
+		CheckEquals(2, (result as TJsonObject).Count, 'result.count');
+		CheckEqualsString('b', (result as TJsonObject).GetValue<String>('a'), 'a');
+		CheckEqualsString('c', (result as TJsonObject).GetValue<String>('b'), 'b');
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePatch_AddsPair_ExistingNullValue();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+	resultObj: TJsonObject;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('{"e":null}');
+		patch := TJSONObject.ParseJSONValue('{"a":1}');
+		result := TJSONHelper.MergePatch(target, patch);
+		Check(result <> patch, 'result <> patch');
+		Check(result <> target, 'result <> target');
+		Check(result is TJsonObject, 'result is object');
+		resultObj := (result as TJsonObject);
+
+		CheckEquals(2, resultObj.Count, 'obj.count');
+		Check(resultObj.GetValue('e').Null, 'obj.e is null');
+		CheckEquals(1, resultObj.GetValue<Integer>('a'));
+		//   {"e":null}      {"a":1}         {"e":null,
+		//                                    "a":1}
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePatch_RemovesEverything();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('{"a":"foo"}');
+		patch := TJSONObject.ParseJSONValue('null');
+		result := TJSONHelper.MergePatch(target, patch);
+		Check(result <> patch, 'result <> patch');
+		Check(result <> target, 'result <> target');
+		Check(result is TJSONNull, 'result is TJSONNull');
+		//   {"a":"foo"}     null            null
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePatch_RemovesNullValues1();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('{"a":"b"}') as TJsonObject;
+		patch := TJSONObject.ParseJSONValue('{"a":null}') as TJsonObject;
+		result := TJSONHelper.MergePatch(target, patch);
+		Check(result <> patch, 'result <> patch');
+		Check(result <> target, 'result <> target');
+		Check(result is TJsonObject, 'result is TJsonObject');
+		CheckEquals(0, (result as TJsonObject).Count);
+		//{"a":"b"}       {"a":null}      {}
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePatch_RemovesNullValues2();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('{"a":"b", "b":"c"}') as TJsonObject;
+		patch := TJSONObject.ParseJSONValue('{"a":null}') as TJsonObject;
+		result := TJSONHelper.MergePatch(target, patch);
+		Check(result <> patch);
+		Check(result <> target);
+		Check(result is TJsonObject);
+		CheckEquals(1, (result as TJsonObject).Count);
+		Check((result as TJsonObject).GetValue<String>('b') = 'c');
+		//{"a":"b", "b": "c"}       {"a":null}      {"b": "c"}
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePatch_RemoveSomethingThatDoesNotExist();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+	obj: TJsonObject;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('{"a":{"b": "c"}}') as TJsonObject;
+		patch := TJSONObject.ParseJSONValue('{"a":{"b": "d", "c": null}}') as TJsonObject;
+		result := TJSONHelper.MergePatch(target, patch);
+
+		Check(result <> patch); Check(result <> target);
+		Check(result is TJsonObject);
+		CheckEquals(1, (result as TJsonObject).Count);
+
+		obj := (result as TJSONObject).GetValue<TJSONObject>('a');
+		CheckNotNull(obj);
+		CheckEquals(1, obj.Count);
+		CheckEqualsString('d', obj.GetValue<String>('b'));
+
+		// {"a": {         {"a": {         {"a": {
+		//     "b": "c"}       "b": "d",       "b": "d"
+		//   }                 "c": null}      }
+		//				   }               }
+
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePatch_RemovesSomethingThatDoesNotExist2;
+var
+	target, patch: TJsonValue;
+	expected: TJsonObject;
+	actual: TJsonValue;
+begin
+	target := nil; patch := nil; expected := nil; actual := nil;
+	try
+		target := TJSONObject.Create();
+		patch := TJSONObject.ParseJSONValue('{"a": {"bb": {"ccc": null}}}');
+
+		expected := TJsonObject.ParseJSONValue('{"a": {"bb": {}}}') as TJsonObject;
+		actual := TJSONHelper.MergePatch(target, patch);
+
+		CheckIs(actual, TJsonObject);
+		Check( TJSONHelper.Equals(expected, actual) );
+
+		//   {}              {"a":            {"a":
+		//                    {"bb":           {"bb":
+		//                     {"ccc":          {}}}
+		//					  null}}}
+	finally
+		target.Free(); patch.Free();
+		expected.Free(); actual.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePatch_ReplacesArray();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('{"a":["b"]}') as TJsonObject;
+		patch := TJSONObject.ParseJSONValue('{"a": "c"}') as TJsonObject;
+		result := TJSONHelper.MergePatch(target, patch);
+
+		Check(target <> patch);
+		Check(result is TJsonObject);
+		CheckEquals(1, (result as TJsonObject).Count);
+		CheckEqualsString('c', (result as TJsonObject).GetValue<String>('a'));
+		//   {"a":["b"]}     {"a":"c"}       {"a":"c"}
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePatch_ReplacesArrayWithArray;
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+	arr: TJsonArray;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('{"a": [{"b":"c"}]}');
+		patch := TJsonObject.ParseJSONValue('{"a": [1]}');
+		result := TJsonHelper.MergePatch(target, patch);
+
+		Check(result <> patch); Check(result <> target);
+		Check(result is TJSONObject);
+		CheckEquals(1, (result as TJSONObject).Count, 'object.count');
+		CheckIs((result as TJsonObject).GetValue('a'), TJSONArray, 'a is array');
+		arr := (result as TJsonObject).GetValue('a') as TJsonArray;
+		CheckEquals(1, arr.Count, 'array.count');
+		CheckEquals(1, arr.Items[0].GetValue<Integer>(), 'array[0]');
+
+		//   {"a": [         {"a": [1]}      {"a": [1]}
+		//     {"b":"c"}
+		//    ]}
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePatch_ReplacesObjectWithString();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('{"a":"foo"}') as TJsonObject;
+		patch := TJSONObject.ParseJSONValue('"bar"') as TJSONString;
+		result := TJSONHelper.MergePatch(target, patch);
+
+		Check(result <> patch);
+		Check(result is TJSONString);
+		CheckEqualsString('bar', (result as TJSONString).Value());
+		//   {"a":"foo"}     "bar"           "bar"
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePatch_ReplacesValue();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('{"a":"b"}') as TJsonObject;
+		patch := TJSONObject.ParseJSONValue('{"a": "c"}') as TJsonObject;
+		result := TJSONHelper.MergePatch(target, patch);
+
+		Check(result <> patch); Check(result <> target);
+		Check(result is TJsonObject);
+		CheckEquals(1, (result as TJsonObject).Count);
+		CheckEqualsString('c', (result as TJsonObject).GetValue<String>('a'));
+		//   {"a":"b"}       {"a":"c"}       {"a":"c"}
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePatch_ReplacesValueWithArray();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+	arr: TJsonArray;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('{"a":"c"}');
+		patch := TJSONObject.ParseJSONValue('{"a": ["b"]}');
+		result := TJSONHelper.MergePatch(target, patch);
+
+		Check(result <> patch); Check(result <> target);
+		Check(result is TJsonObject);
+		CheckEquals(1, (result as TJsonObject).Count);
+		CheckIs((result as TJsonObject).Values['a'], TJSONArray, 'a is array');
+		arr := (result as TJsonObject).Values['a'] as TJSONArray;
+		CheckEquals(1, arr.Count, 'array.count');
+		CheckIs(arr.Items[0], TJsonString);
+		CheckEqualsString('b', arr.Items[0].GetValue<String>());
+		//   {"a":"c"}       {"a":["b"}}       {"a":["b"]}
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePath_ReplacesArrayWithObject();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('[1,2]');
+		patch := TJSONObject.ParseJSONValue('{"a": "b", "c": null}');
+		result := TJSONHelper.MergePatch(target, patch);
+
+		Check(result <> patch); Check(result <> target);
+		Check(result is TJsonObject, 'result is object');
+		CheckEquals(1, (result as TJsonObject).Count, 'object.count');
+		CheckEqualsString('b', (result as TJSONObject).GetValue<String>('a'));
+		//   [1,2]           {"a":"b",       {"a":"b"}
+		//                    "c":null}
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePath_ReplacesArrayWithOtherArray();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+	arr: TJsonArray;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('["a","b"]');
+		patch := TJSONObject.ParseJSONValue('["c","d"]');
+		result := TJSONHelper.MergePatch(target, patch);
+
+		Check(result <> patch); Check(result <> target);
+		Check(result is TJSONArray, 'result is array');
+		arr := (result as TJSONArray);
+
+		CheckEquals(2, arr.Count, 'array.count');
+		CheckEqualsString('c', arr.Items[0].GetValue<String>());
+		CheckEqualsString('d', arr.Items[1].GetValue<String>());
+		//   ["a","b"]       ["c","d"]       ["c","d"]
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
+end;
+
+procedure TestTJsonHelper.MergePath_ReplacesObjectWithArray();
+var
+	target, patch: TJsonValue;
+	result: TJsonValue;
+begin
+	target := nil; patch := nil; result := nil;
+	try
+		target := TJSONObject.ParseJSONValue('{"a": "b"}');
+		patch := TJSONObject.ParseJSONValue('["c"]');
+		result := TJSONHelper.MergePatch(target, patch);
+
+		Check(result <> patch);
+		Check(result is TJSONArray);
+		CheckEquals(1, (result as TJSONArray).Count, 'array.count');
+		CheckEqualsString('c', (result as TJsonArray).Items[0].GetValue<String>());
+		//   {"a":"b"}       ["c"]           ["c"]
+	finally
+		target.Free(); patch.Free(); result.Free();
+	end;
 end;
 
 procedure TestTJsonHelper.ArrayEquals_Integer();
